e01ca9b02a99997b1ea363bd10c1673502b10385
Sebastian Pütz <sebastian.puetz@uni-tuebingen.de>
2020-05-06 12:35:46 +0200

    Fix folder structure.

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ need_cython = not all([os.path.exists(p) for p in
 
 if need_cython:
     from Cython.Build import cythonize
-    extensions = cythonize([Extension("ffp.subwords.hash_indexers", ["ffp/subwords/hash_indexers.pyx"]),
-                            Extension("ffp.subwords.ngrams", ["ffp/subwords/ngrams.pyx"]),
-                            Extension("ffp.subwords.explicit_indexer", ["ffp/subwords/explicit_indexer.pyx"])])
+    extensions = cythonize([Extension("ffp.subwords.hash_indexers", ["src/ffp/subwords/hash_indexers.pyx"]),
+                            Extension("ffp.subwords.ngrams", ["src/ffp/subwords/ngrams.pyx"]),
+                            Extension("ffp.subwords.explicit_indexer", ["src/ffp/subwords/explicit_indexer.pyx"])])
 else:
-    extensions = [Extension("ffp.subwords.hash_indexers", ["ffp/subwords/hash_indexers.c"]),
-                  Extension("ffp.subwords.explicit_indexer", ["ffp/subwords/explicit_indexer.c"]),
-                  Extension("ffp.subwords.ngrams", ["ffp/subwords/ngrams.c"])]
+    extensions = [Extension("ffp.subwords.hash_indexers", ["src/ffp/subwords/hash_indexers.c"]),
+                  Extension("ffp.subwords.explicit_indexer", ["src/ffp/subwords/explicit_indexer.c"]),
+                  Extension("ffp.subwords.ngrams", ["src/ffp/subwords/ngrams.c"])]
 
 NAME = 'ffp'
 setup(
@@ -31,9 +31,10 @@ setup(
     description="Interface to finalfusion embeddings",
     ext_modules=extensions,
     license='BlueOak-1.0.0',
-    packages=find_packages(),
+    packages=find_packages('src'),
     install_requires=["numpy", "toml"],
     include_package_data=True,
     package_data={'': ['*.pyx']},
+    package_dir={'': 'src'},
     url="https://github.com/sebpuetz/ffp"
 )
